61471ab2266142db951b438b9d9b8a8e806f6da7
Kai Gu <kai.gu@mail.utoronto.ca>
2020-05-13 20:32:26 -0400

    Change plot for plotting coherence

--- liouville_equation/plot.py
+++ liouville_equation/plot.py
@@ -6,6 +6,8 @@ from matplotlib import animation
 from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
 
+NUMPES = 2 # number of potential energy surfaces
+
 # read input grid and time
 def read_input():
     xfile = open('x.txt', 'r')
@@ -21,20 +23,24 @@ def read_input():
 
 # plot preparation
 x, p, t = read_input()
-dx = (x[len(x)-1] - x[0]) / (len(x) - 1)
-dp = (p[len(p)-1] - p[0]) / (len(p) - 1)
+LEN_X = len(x)
+LEN_P = len(p)
+LEN_T = len(t)
+dx = (x[LEN_X-1] - x[0]) / (LEN_X - 1)
+dp = (p[LEN_P-1] - p[0]) / (LEN_P - 1)
 xv, pv = np.meshgrid(x, p) # transform to vector for plotting
 # pick the desired colormap, sensible levels, and define a normalization
 # instance which takes data values and translates those into levels.
-xmax00 = 0.2
-xmax11 = 0.05
-levels00 = MaxNLocator(nbins=15).tick_values(-xmax00, xmax00) # color region
-levels11 = MaxNLocator(nbins=15).tick_values(-xmax11, xmax11) # color region
+xmax = [[0.2, 0.1], [0.1, 0.05]]
+levels= [[], []]
+for i in range(NUMPES):
+    for j in range(NUMPES):
+        levels[i].append(MaxNLocator(nbins=15).tick_values(-xmax[i][j],xmax[i][j])) # color region
 cmap = plt.get_cmap('seismic') # the kind of color: red-white-blue
 #norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True) # the mapping rule
 
 # initialize the plot
-fig, (ax0, ax1) = plt.subplots(nrows=2)
+fig, axs = plt.subplots(nrows=NUMPES, ncols=NUMPES, figsize=(120,60))
 
 # set time text
 time_template = 'time = %da.u.'
@@ -42,22 +48,24 @@ time_template = 'time = %da.u.'
 # initialization: blank page
 def init():
     # clear, then set x/y label, title of subplot, and colorbar
-    ax0.clear()
-    ax0.set_xlabel('x')
-    ax0.set_ylabel('p')
-    ax0.set_title(r'$\rho_{00}$')
-    cf0 = ax0.contourf(xv, pv, np.zeros((len(x), len(p))), levels=levels00, cmap=cmap)
-    fig.colorbar(cf0, extend='both', ax=ax0)
-    ax1.clear()
-    ax1.set_xlabel('x')
-    ax1.set_ylabel('p')
-    ax1.set_title(r'$\rho_{11}$')
-    cf1 = ax1.contourf(xv, pv, np.zeros((len(x), len(p))), levels=levels11, cmap=cmap)
-    cf1.set_clim(-xmax11, xmax11)
-    fig.colorbar(cf1, extend='both', ax=ax1)
+    cfs = [[], []]
+    for i in range(NUMPES):
+        for j in range(NUMPES):
+            axs[i][j].clear()
+            axs[i][j].set_xlabel('x')
+            axs[i][j].set_ylabel('p')
+            if i == j:
+                axs[i][j].set_title(r'$\rho_{%d%d}$' % (i, j))
+            elif i < j:
+                axs[i][j].set_title(r'$\Re(\rho_{%d%d})$' % (i, j))
+            else:
+                axs[i][j].set_title(r'$\Im(\rho_{%d%d})$' % (j, i))
+            cfs[i].append(axs[i][j].contourf(xv, pv, np.zeros((LEN_X,LEN_P)), levels=levels[i][j], cmap=cmap))
+            fig.colorbar(cfs[i][j], extend='both', ax=axs[i][j])
+            cfs[i][j].set_clim(-xmax[i][j], xmax[i][j])
     # figure settings: make them closer, title to be time
     fig.suptitle('')
-    return fig, ax0, ax1,
+    return fig, axs,
 
 # animation: each timestep in phase.txt
 def ani(i):
@@ -65,32 +73,36 @@ def ani(i):
     file = open('phase.txt', 'r')
     # old data
     for j in range(i - 1):
-        file.readline() # rho[0][0]
-        file.readline() # rho[0][1]
-        file.readline() # rho[1][0]
-        file.readline() # rho[1][1]
+        for k in range(NUMPES*NUMPES):
+            file.readline() # rho
         file.readline() # blank line
     # new data
-    rho0 = np.array(file.readline().split(), dtype=float)
-    for j in range(len(rho0)//2):
-        rho0[j] = rho0[2 * j]
-    rho0_real = rho0[0:len(rho0)//2].reshape(len(p),len(x)).T
-    file.readline() # rho[0][1]
-    file.readline() # rho[1][0]
-    rho1 = np.array(file.readline().split(), dtype=float)
-    for j in range(len(rho1)//2):
-        rho1[j] = rho1[2 * j]
-    rho1_real = rho1[0:len(rho1)//2].reshape(len(p),len(x)).T
+    rho = []
+    for j in range(NUMPES*NUMPES):
+        rho.append(np.array(file.readline().split(), dtype=float))
+    LENGTH = len(rho[0])
+    rho = np.reshape(np.asarray(rho),(NUMPES,NUMPES,LENGTH))
+    for j in range(LENGTH//2):
+        for k in range(NUMPES):
+            rho[k,k,j] = rho[k,k,2*j]
+            for l in range(k+1,NUMPES):
+                rho[k,l,j] = (rho[k,l,2*j]+rho[l,k,2*j])/2.0
+                rho[l,k,j] = (rho[k,l,2*j+1]-rho[l,k,2*j+1])/2.0
+    rhoi = [[],[]]
+    for j in range(NUMPES):
+        for k in range(NUMPES):
+            rhoi[j].append(rho[j,k,0:LENGTH//2].reshape(LEN_P,LEN_X).T)
     file.close()
 
     # print contourfs
-    cf0 = ax0.contourf(xv, pv, rho0_real, levels=levels00, cmap=cmap)
-    cf1 = ax1.contourf(xv, pv, rho1_real, levels=levels11, cmap=cmap)
+    for j in range(NUMPES):
+        for k in range(NUMPES):
+            axs[j][k].contourf(xv, pv, rhoi[j][k], levels=levels[j][k], cmap=cmap)
     fig.suptitle(time_template % t[i])
-    return fig, ax0, ax1,
+    return fig, axs,
 
 # make the animation
-ani = animation.FuncAnimation(fig, ani, len(t), init, interval=10000//(t[1]-t[0]), repeat=False, blit=False)
+ani = animation.FuncAnimation(fig, ani, LEN_T, init, interval=10000//(t[1]-t[0]), repeat=False, blit=False)
 # show
 ani.save('phase.gif','imagemagick')
 # plt.show()
@@ -100,7 +112,7 @@ ani.save('phase.gif','imagemagick')
 def calc_pop():
     file = open('phase.txt', 'r')
     ppl = [[],[]]
-    for i in range(len(t)):
+    for i in range(LEN_T):
         ppl[0].append(sum(np.array(file.readline().split(), dtype=float)) * dx * dp)
         file.readline() # rho[0][1]
         file.readline() # rho[1][0]
@@ -114,6 +126,6 @@ ppl = calc_pop()
 plt.plot(t, ppl[0], color='r', label='Population[0]')
 plt.plot(t, ppl[1], color='b', label='Population[1]')
 plt.legend(loc = 'best')
-plt.xlim((t[0],t[len(t)-1]))
+plt.xlim((t[0],t[LEN_T-1]))
 plt.ylim((0,1))
 plt.savefig('phase.png')
